57f05f028d2cc809156ff73c677816d8207a76da
Tesqvido <sxar.1234t@gmail.com>
2025-03-15 16:26:58 +0100

    Update Test.php

--- Test.php
+++ Test.php
@@ -2,9 +2,9 @@
 ob_start();
 error_reporting(0);
 
-$client_id     = ""; //CLIENT ID HERE
-$client_secret = ""; //CLIENT SECRET HERE
-$redirect      = ""; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
+$client_id     = "1350488480270254110"; //CLIENT ID HERE
+$client_secret = "6xyjh2NQbjY2c0v0-epTLaVZNdcu7KFT"; //CLIENT SECRET HERE
+$redirect      = "https://red-guard.vercel.app/callback.php"; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
 
 function get_ip_address()
 {
